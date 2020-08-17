a5ce3df9e5490c9aceabb91cdf7395a1fe5d6105
Sharon Kuninin <schnitzelftw@gmail.com>
2020-08-17 23:48:17 +0300

    Add documentations to the threat classes

--- algorithmics/enemy/asteroids_zone.py
+++ algorithmics/enemy/asteroids_zone.py
@@ -7,4 +7,11 @@ from algorithmics.utils.coordinate import Coordinate
 
 @dataclass
 class AsteroidsZone(Threat):
+    """Asteroids-attacked zone
+
+    An asteroids-attacked zone (or zone for short) is an area (polygon) in space that has high probability of non-minor
+    asteroids circling inside them. Because of the high probability of them being there, and the fact that interaction
+    between the spaceship and them is very deadly, the spaceship must never (!!!) enter the boundary of those zones.
+    """
+
     boundary: List[Coordinate]
--- algorithmics/enemy/threat.py
+++ algorithmics/enemy/threat.py
@@ -4,8 +4,14 @@ from dataclasses import dataclass
 
 @dataclass
 class Threat(ABC):
-    """Generic threat class
+    """Generic kidnappers threat class
 
-    Common abstract or concrete helper functions can be implemented here.
+    All threat classes inherit from this base class and define objects in space that our spaceship must not be detected
+    by.
+
+    For an explanation about the different types of threats and their behavior, refer to this document:
+        https://docs.google.com/document/d/16ps7VZRZd3gsKrWACelI3DyU_dNB8ET3lGMScaewams/edit?usp=sharing
     """
+
+    # Feel free to add & implement common methods for threats here
     pass
