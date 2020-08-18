f7ea41d04745eac01d71c7a970054e93ffa43d3d
Sharon Kuninin <schnitzelftw@gmail.com>
2020-08-18 12:38:09 +0300

    Refer to the drive document in the navigate function

--- algorithmics/navigator.py
+++ algorithmics/navigator.py
@@ -12,6 +12,9 @@ def navigate(source: Coordinate, target: Coordinate, threats: List[Threat]) -> L
 
     Note: The path must start at the source coordinate and end at the target coordinate!
 
+    For more details about the problem and its requirements, refer to the following document:
+        https://docs.google.com/document/d/16ps7VZRZd3gsKrWACelI3DyU_dNB8ET3lGMScaewams/edit?usp=sharing
+
     :param source: source coordinate of the spaceship
     :param target: target coordinate of the spaceship
     :param threats: list of threats in the scenario (might include any combination of observation posts, asteroids zones
